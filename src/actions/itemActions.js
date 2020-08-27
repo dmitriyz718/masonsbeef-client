845e73bfccdebe5b2cd9b46541104c0f9368b4d4
u101004132 <dzag17188@gmail.com>
2020-08-26 22:50:09 -0400

    start with create react app

--- src/actions/itemActions.js
+++ src/actions/itemActions.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 
 export const getProducts = () => dispatch => {
     dispatch(setItemsLoading());
-    axios.get(`${REACT_APP_API_URL}/api/products`)
+    axios.get(`${process.env.REACT_APP_API_URL}/api/products`)
         .then(res => dispatch({
             type: GET_ITEMS,
             payload: res.data
@@ -17,7 +17,7 @@ export const getProducts = () => dispatch => {
 // single
 export const getProduct = (id) => dispatch => {
     dispatch(setItemsLoading());
-    axios.get(`${REACT_APP_API_URL}/api/products/view/${id}`)
+    axios.get(`${process.env.REACT_APP_API_URL}/api/products/view/${id}`)
         .then(res => dispatch({
             type: GET_ITEM,
             payload: res.data
@@ -27,7 +27,7 @@ export const getProduct = (id) => dispatch => {
 export const addProduct = (item) => (dispatch, getState) => {
     dispatch(setItemsLoading());
     axios
-        .post(`${REACT_APP_API_URL}/api/products`, item, tokenConfig(getState))
+        .post(`${process.env.REACT_APP_API_URL}/api/products`, item, tokenConfig(getState))
         .then(res => dispatch({
             type: ADD_ITEM,
             payload: res.data
@@ -37,7 +37,7 @@ export const addProduct = (item) => (dispatch, getState) => {
 // delete
 export const deleteProduct = (id) => (dispatch, getState) => {
     axios
-        .delete(`${REACT_APP_API_URL}/api/products/delete/${id}`, tokenConfig(getState)).then(res => dispatch({
+        .delete(`${process.env.REACT_APP_API_URL}/api/products/delete/${id}`, tokenConfig(getState)).then(res => dispatch({
             type: DELETE_ITEM,
             payload: id
         }))
@@ -46,7 +46,7 @@ export const deleteProduct = (id) => (dispatch, getState) => {
 // update
 export const updateProduct = (id, data) => (dispatch, getState) => {
     axios
-        .put(`${REACT_APP_API_URL}/api/products/update/${id}`, data, tokenConfig(getState)).then(res => dispatch({
+        .put(`${process.env.REACT_APP_API_URL}/api/products/update/${id}`, data, tokenConfig(getState)).then(res => dispatch({
             type: UPDATE_ITEM,
             payload: id
         }))
@@ -55,7 +55,7 @@ export const sendEmail = (emailData) => dispatch => {
     dispatch(setItemsLoading());
     axios({
         method: "POST",
-        url: `${REACT_APP_API_URL}/api/products/send`,
+        url: `${process.env.REACT_APP_API_URL}/api/products/send`,
         body: emailData
     }).then(res => dispatch({
         type: EMAIL_SENT,
