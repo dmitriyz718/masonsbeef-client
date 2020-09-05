caa416407a303c45ba07b3097ff17ac024b1e66a
u101004132 <dzag17188@gmail.com>
2020-09-05 19:47:54 -0400

    added about us

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,27 +1,32 @@
-import React, { Component } from 'react'
-import { Alert } from 'reactstrap';
-import './about.css'
+import React, { Component } from "react";
+import { Alert } from "reactstrap";
+import "./about.css";
 class About extends Component {
-    render() {
-        return (
-            <div className="aboutDiv">
-                <Alert color="info">
-                    <h4 className="alert-heading">"Get more for your money"</h4>
-                    <p>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda ipsum perferendis esse nobis est libero eum perspiciatis sit maxime mollitia nam impedit unde eligendi rerum magni amet, repellat nisi molestiae.
-                        Veniam culpa officiis praesentium dolore modi pariatur, nesciunt facilis autem itaque minima nostrum ipsa explicabo ratione optio, maiores expedita deserunt accusantium ipsum, cumque enim in cum voluptatum? Exercitationem, fugiat nam.
-                        Corporis sit voluptatum explicabo, provident id nihil similique impedit, tempore atque officia cum autem quia obcaecati assumenda laudantium, totam facilis enim. Maxime, magni dignissimos sit iure odit quos. Iste, commodi.
-                        Rerum voluptate tempora minus possimus dolorem ipsam sequi quam cupiditate nisi? Delectus, ab mollitia? Tenetur ut assumenda veritatis atque unde odio, repellat, itaque ad esse tempora dolorum. Repellendus, eveniet nostrum?
-                        Tenetur perferendis fugit modi illum dolor blanditiis nam maiores voluptates magni consequuntur optio, aperiam nemo fugiat quasi quaerat exercitationem perspiciatis officiis officia, eaque eum ducimus odio rerum sit reprehenderit? Fuga!
-                        Eligendi rerum doloribus architecto ipsa? Distinctio nemo atque placeat quae dolorum maxime beatae reprehenderit minima velit molestias, delectus nisi veritatis rem fugit nihil dolore id quis ea voluptates hic expedita?
-                        </p>
-                    <hr />
-                    <p className="mb-0">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quibusdam fuga id nobis voluptas placeat dolor minus nemo obcaecati eos, quaerat maxime ipsam maiores quia delectus ipsum. Totam, architecto harum!
-                        </p>
-                </Alert>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="aboutDiv">
+        <Alert color="info">
+          <h4 className="alert-heading">Quality You Can trust</h4>
+          <p>
+            What started out as just a hobby, turned into allowing me to be a
+            stay at home mom to two amazing children. While I started making
+            jerky in 2015 it wasn't until my son Mason was born in 2017 that I
+            started selling to friends and family. Born with a rare illness;
+            Malrotation of the Intestines, Mason required 3 major surgeries and
+            a 4 month hospital stay. Now mason is a thriving three year old.
+            Masons beef is also thriving three years later, being we only choose
+            the finest meats and freshest seasonings so we know we are giving
+            our customers the best there is! Every order is custom made and
+            vacume sealed so they arrive quick and fresh!
+          </p>
+          <hr />
+          <p className="mb-0">
+            Feel free to browse our selection of products, and give us a try!
+            You wont regret it!
+          </p>
+        </Alert>
+      </div>
+    );
+  }
 }
 export default About;
